f31872d60c38232d2d0d141b8e70f73c3fd69f80
samstoia <samstoia@gmail.com>
2019-05-15 08:35:15 -0700

    Updates

---
+++ precache-manifest.99195444d6b3cd4e7a390b0fd71b27f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e07517df6bc8934adaef0a46b27ab6d7",
+    "url": "/new-tap-room/index.html"
+  },
+  {
+    "revision": "54e757a290e346d2b4ab",
+    "url": "/new-tap-room/static/js/2.2df884ed.chunk.js"
+  },
+  {
+    "revision": "f37914b3ce02baf232a2",
+    "url": "/new-tap-room/static/js/main.839cc1ca.chunk.js"
+  },
+  {
+    "revision": "983fb52d9a99a02948ee",
+    "url": "/new-tap-room/static/js/runtime~main.4371e51a.js"
+  },
+  {
+    "revision": "794f3374b3a5ffbf32ad827b42a9aa59",
+    "url": "/new-tap-room/static/media/beer.794f3374.jpg"
+  },
+  {
+    "revision": "04a77c029338c305f93a2c8dcc30b76f",
+    "url": "/new-tap-room/static/media/story.04a77c02.jpeg"
+  },
+  {
+    "revision": "0bb97cbfddebe28c3c3037460c36d0a0",
+    "url": "/new-tap-room/static/media/taproom.0bb97cbf.jpeg"
+  }
+]);
